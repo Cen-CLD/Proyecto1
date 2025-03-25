aab32b3e9f4d238b08d3fe02a187f4ce6533e06d
RicarTecFo <rmurillopi@ucenfotec.ac.cr>
2025-03-25 13:56:23 -0600

    avance 1, priorizar y visualizar estado

--- Front-End/assets/js/councilor/main.js
+++ Front-End/assets/js/councilor/main.js
@@ -2,10 +2,32 @@ let modal = document.getElementById("modalInitiatives");
 let openModalBtn = document.getElementById("btnOpenModalInitiatives");
 let closeModalBtn = document.getElementById("btnCloseModalInitiatives");
 
+let viewStatusBtn = document.getElementById("btnViewStatus");
+let prioritizeBtn = document.getElementById("btnPrioritize");
+
 function openModal() {
     modal.style.display = "block";
 }
 
 function closeModal() {
     modal.style.display = "none";
+}
+
+viewStatusBtn.addEventListener("click", viewStatus);
+prioritizeBtn.addEventListener("click", prioritizeInitiative);
+
+function viewStatus() {
+    Swal.fire({
+        icon: "warning",
+        title: "No revisado",
+        text: "La iniciativa aún está pendiente."
+      });
+}
+
+function prioritizeInitiative() {
+    Swal.fire({
+        icon: "success",
+        title: "Éxito",
+        text: "La iniciativa ha sido priorizada."
+      });
 }
